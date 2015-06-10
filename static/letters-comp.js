34490a8808c345b09b9b76876f626dca0c5de6b8
wangchj <cjw39@hotmail.com>
2015-06-10 10:52:10 -0500

    Move shuffle(array) function to generic-comp.js

--- static/letters-comp.js
+++ static/letters-comp.js
@@ -1,22 +1,3 @@
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
 var SingleLetterSlide = React.createClass({
     render: function() {
         return (<div className="flash-letter">{this.props.letter}</div>);
