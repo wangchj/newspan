c3e4318572acf11cecc197cec9985b903c58e315
wangchj <cjw39@hotmail.com>
2015-07-27 11:49:19 -0500

    Move hasPoint() and shuffle(array) from js/comp/generic.js to js/util.js. Close issues #24.

--- web/js/comp/generic.js
+++ web/js/comp/generic.js
@@ -1,22 +1,3 @@
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
 var Instruction = React.createClass({
     getDefaultProps: function() {
         return {
@@ -283,23 +264,3 @@ var LowTra = React.createClass({
         );
     }
 });
-
-/**
- * Checks if the array of points contains a specific point.
- * @param array array<array>   An array of array which is a point.
- * @param point array<integer> An array containing two elements [x, y].
- * @return true if array contains the point.
- *         false if:
- *             array is null or undefined
- *             point is null or undefined
- *             array does not contain the point
- */
-function arrayHasPoint(array, point) {
-    if(!array || !point)
-        return false;
-
-    for(var i = 0; i < array.length; i++)
-        if(array[i] && array[i][0] == point[0] && array[i][1] == point[1])
-            return true;
-    return false;
-}
