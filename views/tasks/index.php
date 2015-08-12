5d45dea63e2a425e29c0cef3d2a6ab29dcbacd27
wangchj <cjw39@hotmail.com>
2015-08-12 09:42:45 -0500

    Change user interaction so task details are shown when user click on a row in the task list of task index page.

--- views/tasks/index.php
+++ views/tasks/index.php
@@ -6,21 +6,45 @@ use yii\helpers\Url;
 $this->title = 'Tasks';
 ?>
 
+<style>
+    tbody > tr {
+        cursor: pointer;
+    }
+</style>
+
 <h1>Tasks</h1>
 
-<table class="table">
-    <tr>
-        <th>#</th>
-        <th>Name</th>
-        <th>Created Date</th>
-        <th>Responses</th>
-    </tr>
-    <?php foreach($tasks as $task): ?>
+<table class="table table-hover">
+    <thead>
         <tr>
-            <td><?=$task->taskId?></td>
-            <td><a href="<?=Url::to(['tasks/view', 'taskId'=>$task->taskId])?>"><?=$task->name?></a></td>
-            <td><?=(new DateTime($task->createTime))->format('Y-m-d H:i:s')?></td>
-            <td><?=count($task->responses)?></td>
+            <th>#</th>
+            <th>Name</th>
+            <th>Created Date</th>
+            <th>Responses</th>
         </tr>
-    <?php endforeach;?>
+    </thead>
+    <tbody>
+        <?php foreach($tasks as $task): ?>
+            <tr data-taskid="<?=$task->taskId?>">
+                <td><?=$task->taskId?></td>
+                <td><?=$task->name?></td>
+                <td><?=(new DateTime($task->createTime))->format('Y-m-d H:i:s')?></td>
+                <td><?=count($task->responses)?></td>
+            </tr>
+        <?php endforeach;?>
+    </tbody>
 </table>
+
+<?php $this->beginBlock('TheEnd');?>
+    <script type="text/javascript">
+        var viewUrl = '<?=Url::to(['tasks/view', 'taskId'=>0])?>';
+    </script>
+    <script type="text/javascript">
+        $(function(){
+            $('tbody tr').click(function(event){
+                var id = $(this).data('taskid');
+                window.location.href = viewUrl.replace('0', id);
+            });
+        });
+    </script>
+<?php $this->endBlock();?>
