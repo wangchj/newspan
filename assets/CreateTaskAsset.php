7b1f1ad2d54c991ad6ef89d29071ce2026f839f3
wangchj <cjw39@hotmail.com>
2015-08-17 17:15:52 -0500

    Remove in-browser JSX transformer.

--- assets/CreateTaskAsset.php
+++ assets/CreateTaskAsset.php
@@ -12,7 +12,6 @@ class CreateTaskAsset extends AssetBundle
     ];
     public $js = [
         'https://fb.me/react-0.13.3.js',
-        'https://fb.me/JSXTransformer-0.13.3.js',
         'js/util.js',
         'js/models.js'
     ];
