019eb69fbf62936b6691a7173ce7cd8ad34f419f
wangchj <cjw39@hotmail.com>
2016-04-06 16:04:55 -0500

    Ability to add new languages when creating a task.

--- web/js/comp-src/explode-task.js
+++ web/js/comp-src/explode-task.js
@@ -2,12 +2,21 @@ var ExplodeTask = React.createClass({
     propTypes: {
         task: React.PropTypes.object.isRequired
     },
+    getInitialState: function() {
+        return {
+            lang: Object.keys(this.props.task.instructs)[0]
+        };
+    },
     nop: function() {
+    },
+    onLangClick: function(lang) {
+        this.setState({lang: lang});
     },
     render: function() {
         return (
             <div>
-                <TaskObList task={this.props.task} mode='view' onAddProbClick={this.nop} onProbEdit={this.nop} onProbDel={this.nop} onBlockDel={this.nop} onInstEdit={this.nop} onInstDel={this.nop}/>
+                <CreateTask.LangPane task={this.props.task} lang={this.state.lang} onLangClick={this.onLangClick} onAddLangClick={this.nop}/>
+                <TaskObList task={this.props.task} mode='view' onAddProbClick={this.nop} onProbEdit={this.nop} onProbDel={this.nop} onBlockDel={this.nop} onInstEdit={this.nop} onInstDel={this.nop} lang={this.state.lang}/>
             </div>
         );
     }
